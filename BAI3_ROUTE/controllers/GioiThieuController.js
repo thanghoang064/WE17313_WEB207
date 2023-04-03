ac9908a51462041e1fb20b841ab7608d6aa4a13a
ITPLUS Academy <thanghoang064@gmail.com>
2023-04-03 07:37:38 +0700

    update project

--- BAI3_ROUTE/controllers/GioiThieuController.js
+++ BAI3_ROUTE/controllers/GioiThieuController.js
@@ -1,15 +1,28 @@
-window.GioiThieuController = function($scope,$routeParams) {
+window.GioiThieuController = function($scope,$routeParams,$http) {
     //$routeParams lấy ra object các tham số trên url
     // console.log($routeParams.name);
     // khởi tạo 1 đối tượng kiểm tra dữ liệu = false 
+    //$http để thao tác với API
     $scope.kiemTraDuLieu = {
         ten:false, // chưa lỗi
         tuoi:false //chưa lỗi 
     }
-    $scope.danhsach = [
-        {id:1,ten:'ABC',tuoi:20},
-        {id:2,ten:'ABCD',tuoi:21}
-    ];
+    //link API khi đón nó về 
+    let apiURL = "http://localhost:3000/hihi";
+    // $scope.danhsach = [
+    //     {id:1,ten:'ABC',tuoi:20},
+    //     {id:2,ten:'ABCD',tuoi:21}
+    // ];
+    // GET data từ API về
+    $scope.getData = function() {
+        $http.get(apiURL).then(function(response){
+            // dữ liệu được đón về thành công dữ liệu sẽ nằm ở biến response
+            // console.log(response.data);
+            $scope.danhsach = response.data;
+        })
+    }
+    //gọi hàm getData
+    $scope.getData();
     $scope.onClose = function() {
         $scope.inputValue = {
             ten:"",
@@ -45,16 +58,30 @@ window.GioiThieuController = function($scope,$routeParams) {
                 return;
             }
             // xử lý thêm 
-            var ds = $scope.danhsach;
+            // var ds = $scope.danhsach;
             //fake id tăng tự động 
-            var newID = ds.length > 0 ? ds[ds.length - 1].id + 1 : 1;
+            // var newID = ds.length > 0 ? ds[ds.length - 1].id + 1 : 1;
             var newItem = {
-                id:newID,
+                // id:newID,
                 ten:$scope.inputValue.ten,
                 tuoi:$scope.inputValue.tuoi
             } //đây là cục đối tượng được thêm mới vào danh sách  
             // push đối tượng mới vào mảng đối tượng danh sách 
-            $scope.danhsach.push(newItem);
+            // $scope.danhsach.push(newItem);
+            // call api với phương thức post để đẩy dữ liệu vào json server
+            $http.post(
+                apiURL,// đường dẫn API 
+                newItem // dữ liệu thêm
+            ).then(
+                function(response) {
+                    // console.log(response);
+                    //nếu như thêm thành công trạng thái status là 201 
+                    if(response.status == 201) {
+                        //gọi lại hàm getData();
+                        $scope.getData();
+                    }
+                }
+            )
             $scope.onClose();
         }
     }
